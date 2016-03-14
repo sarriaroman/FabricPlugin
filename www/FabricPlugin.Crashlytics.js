3e64b111929ca23e23570cebb7f6dc7a6f98f220
nbdamian <damian@ninetyblack.com>
2016-03-15 11:20:42 +1300

    Fixing Transpile issues

--- www/FabricPlugin.Crashlytics.js
+++ www/FabricPlugin.Crashlytics.js
@@ -1,51 +1,72 @@
+'use strict';
 
-class FabricCrashlytics {
-    constructor() {}
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-    addLog(message) {
-        window.fabric.core.execPlugin('addLog', [message]);
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+var FabricCrashlytics = (function () {
+    function FabricCrashlytics() {
+        _classCallCheck(this, FabricCrashlytics);
     }
 
-    sendCrash() {
-        window.fabric.core.execPlugin('sendCrash', []);
-    }
+    _createClass(FabricCrashlytics, [{
+        key: 'addLog',
+        value: function addLog(message) {
+            window.fabric.core.execPlugin('addLog', [message]);
+        }
+    }, {
+        key: 'sendCrash',
+        value: function sendCrash() {
+            window.fabric.core.execPlugin('sendCrash', []);
+        }
+    }, {
+        key: 'sendNonFatalCrash',
+        value: function sendNonFatalCrash(message) {
+            window.fabric.core.execPlugin('sendNonFatalCrash', [message]);
+        }
+    }, {
+        key: 'recordError',
+        value: function recordError(message) {
+            window.fabric.core.execPlugin('recordError', [message]);
+        }
+    }, {
+        key: 'setUserIdentifier',
+        value: function setUserIdentifier(userIdentifier) {
+            window.fabric.core.execPlugin('setUserIdentifier', [userIdentifier]);
+        }
+    }, {
+        key: 'setUserName',
+        value: function setUserName(userName) {
+            window.fabric.core.execPlugin('setUserName', [userName]);
+        }
+    }, {
+        key: 'setUserEmail',
+        value: function setUserEmail(userEmail) {
+            window.fabric.core.execPlugin('setUserEmail', [userEmail]);
+        }
+    }, {
+        key: 'setStringValueForKey',
+        value: function setStringValueForKey(value, key) {
+            window.fabric.core.execPlugin('setStringValueForKey', [value, key]);
+        }
+    }, {
+        key: 'setIntValueForKey',
+        value: function setIntValueForKey(value, key) {
+            window.fabric.core.execPlugin('setIntValueForKey', [value, key]);
+        }
+    }, {
+        key: 'setBoolValueForKey',
+        value: function setBoolValueForKey(value, key) {
+            window.fabric.core.execPlugin('setBoolValueForKey', [value, key]);
+        }
+    }, {
+        key: 'setFloatValueForKey',
+        value: function setFloatValueForKey(value, key) {
+            window.fabric.core.execPlugin('setFloatValueForKey', [value, key]);
+        }
+    }]);
 
-    sendNonFatalCrash(message) {
-        window.fabric.core.execPlugin('sendNonFatalCrash', [message]);
-    }
-
-    recordError(message) {
-        window.fabric.core.execPlugin('recordError', [message]);
-    }
-
-    setUserIdentifier(userIdentifier) {
-        window.fabric.core.execPlugin('setUserIdentifier', [userIdentifier]);
-    }
-
-    setUserName(userName) {
-        window.fabric.core.execPlugin('setUserName', [userName]);
-    }
-
-    setUserEmail(userEmail) {
-        window.fabric.core.execPlugin('setUserEmail', [userEmail]);
-    }
-
-    setStringValueForKey(value, key) {
-        window.fabric.core.execPlugin('setStringValueForKey', [value, key]);
-    }
-
-    setIntValueForKey(value, key) {
-        window.fabric.core.execPlugin('setIntValueForKey', [value, key]);
-    }
-
-    setBoolValueForKey(value, key) {
-        window.fabric.core.execPlugin('setBoolValueForKey', [value, key]);
-    }
-
-    setFloatValueForKey(value, key) {
-        window.fabric.core.execPlugin('setFloatValueForKey', [value, key]);
-    }
-
-}
+    return FabricCrashlytics;
+})();
 
 module.exports = new FabricCrashlytics();
