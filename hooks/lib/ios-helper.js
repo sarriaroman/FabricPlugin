4cff32516c7e8b2932f58f50bafd3fc8c1cfc9f5
Tiago <tiagofly@outlook.com>
2017-02-06 18:41:36 -0200

    Update ios-helper.js

--- hooks/lib/ios-helper.js
+++ hooks/lib/ios-helper.js
@@ -46,7 +46,7 @@ module.exports = {
 //         // "${PODS_ROOT}"
 //         var script = '"' + "./Fabric.framework/run " + pluginConfig.apiKey + " " + pluginConfig.apiSecret + '"';
 //         var script = '"' + "./Fabric.framework/run " + pluginConfig.apiKey + " " + pluginConfig.apiSecret + '"';
-        var script ='"' + '${SRCROOT}' + utilities.getAppName(context) + "/Plugins/cordova-fabric-plugin/Fabric.framework/run" +  pluginConfig.apiKey + " " + pluginConfig.apiSecret + '"';
+        var script ='"' + '${SRCROOT}' + utilities.getAppName(context) + "/Plugins/cordova-fabric-plugin/Fabric.framework/run " +  pluginConfig.apiKey + " " + pluginConfig.apiSecret + '"';
         
         // Generate a unique ID for our new build phase.
         var id = xcodeProject.generateUuid();
